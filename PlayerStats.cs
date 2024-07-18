40ab6a521e40271fb4dbddd2fb697c084ce3ea7f
Lance <99855670+Lancelja@users.noreply.github.com>
2024-07-19 07:53:45 +0800

    Add files via upload

---
+++ PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTestingGround
+{
+    public class PlayerStats
+    {
+        public int p1score { get; set; }
+        public int p2score { get; set; }
+        public int p1life { get; set; }
+        public int p2life { get; set; }
+        public int round { get; set; }
+
+        public string winner { get; set; }
+    }
+}
